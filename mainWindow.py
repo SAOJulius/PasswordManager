2b23c8c3ebdbe65ede00185b99e84e850f01b865
SAOJulius <75691996+SAOJulius@users.noreply.github.com>
2021-12-13 10:23:02 +0100

    .

--- mainWindow.py
+++ mainWindow.py
@@ -1,60 +1,89 @@
 import tkinter as tk
 from csv import writer
-from tkinter import *
-import time
-
-
 
 
 def hauptfenster():
+    AnwendungsMenue = []
 
-    AnwendungsMenü = []
-
-    def arrayfüllen():
-
-        AnwendungsMenü.clear()
+    def optionmenueauffüllen():
+        AnwendungsMenue.clear()
         try:
             with open("Datenbank.csv", "r") as file:
 
                 for line in file:
                     array = line.split(',')
-                    AnwendungsMenü.append(array[2])
+                    AnwendungsMenue.append(array[2])
                 dropdown.place(x=65, y=330)
         except:
             pass
 
-    arrayfüllen()
-
-
     def csvschreiben():
-
         list = [benutzerentry.get(), passwortentry.get(), anwendungsentry.get()]
         with open("Datenbank.csv", "a", newline="") as f:
             writer(f).writerow(list)
-            arrayfüllen()
             benutzerentry.delete(0, "end")
             passwortentry.delete(0, "end")
             anwendungsentry.delete(0, "end")
 
-
-
-    def csvlesen():
-
+    def csvlesen(self):
         with open("Datenbank.csv", "r") as f:
 
             for line in f:
                 array = line.split(',')
-                if str(array[2]) == format(variable.get()):
-
-                    label4.configure(text=array[0])
-                    label5.configure(text=array[0])
+                if str(array[2]) == format(self.variable.get()):
+                    self.label4.configure(text=array[0])
+                    self.label5.configure(text=array[0])
                 else:
                     pass
 
+    def datensatzlöschen():
+        with open("Datenbank.csv", "r") as file:
+            for line in file:
+                array = line.split(',')
+                if array[2] == format(variable.get()):
+                    abfragefenster()
 
-    mainwindow = tk.Tk()
-    mainwindow.geometry('300x500')
-    mainwindow.title("Passwort Manager")
+                else:
+                    pass
+
+    def löschenre():
+        pop.destroy()
+
+    def Löschen():
+        with open("Datenbank.csv", "w") as file:
+            for line in file:
+                array = line.split(',')
+                if array[2] == format(variable.get()):
+                    try:
+                        writer(file).writerow("")
+                        optionmenueauffüllen()
+                    except:
+                        pass
+
+                else:
+                    pass
+
+    optionmenueauffüllen()
+
+    root = tk.Tk()
+    root.geometry('300x500')
+    root.title("Passwort Manager")
+
+    def abfragefenster():
+        global pop
+        pop = tk.Toplevel(root)
+        pop.geometry('250x130')
+        pop.title("Achtung")
+
+        labelfrage = tk.Label(pop, text="Wirklich löschen? ")
+        labelfrage.place(x=70, y=20)
+
+        buttonJa = tk.Button(pop, command=Löschen, text="Ja", height=1, width=5)
+        buttonJa.place(x=55, y=60)
+
+        buttonNein = tk.Button(pop, command=löschenre, text="Nein", height=1, width=5)
+        buttonNein.place(x=145, y=60)
+
 
     label = tk.Label(text="Benutzername: ")
     label.place(x=65, y=30)
@@ -80,13 +109,12 @@ def hauptfenster():
     label3 = tk.Label(text="Anwendung: ")
     label3.place(x=65, y=300)
 
-    variable = tk.StringVar(mainwindow)
-    variable.set(AnwendungsMenü[0])
-    dropdown = tk.OptionMenu(mainwindow, variable, *AnwendungsMenü)
+    variable = tk.StringVar()
+    variable.set(AnwendungsMenue[0])
+    dropdown = tk.OptionMenu(root, variable, *AnwendungsMenue)
     dropdown.place(x=65, y=330)
 
-
-    button2 = tk.Button(command=csvlesen, text="Passwort abfragen", width=20, height=1)
+    button2 = tk.Button(command=csvlesen, text="abfragen", width=7, height=1)
     button2.place(x=65, y=390)
 
     label4 = tk.Label(text="")
@@ -95,8 +123,8 @@ def hauptfenster():
     label5 = tk.Label(text="")
     label5.place(x=65, y=460)
 
+    button3 = tk.Button(command=abfragefenster, text="löschen", width=7, height=1)
+    button3.place(x=150, y=390)
 
+    root.mainloop()
 
-
-
-    mainwindow.mainloop()
